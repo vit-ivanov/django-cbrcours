8d7cca624edb7bbc59ae48ce17fe374151b016b0
Muravskii Yaroslav <myarik@gmail.com>
2014-07-10 18:45:37 +0300

    Fix migration

--- cbrcours/migrations/0001_initial.py
+++ cbrcours/migrations/0001_initial.py
@@ -9,43 +9,43 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'CurrencyLastUpdate'
-        db.create_table(u'cbrcurrencies_currencylastupdate', (
+        db.create_table(u'cbrcource_currencylastupdate', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('last_update', self.gf('django.db.models.fields.DateField')(auto_now_add=True, blank=True)),
         ))
-        db.send_create_signal(u'cbrcurrencies', ['CurrencyLastUpdate'])
+        db.send_create_signal(u'cbrcource', ['CurrencyLastUpdate'])
 
         # Adding model 'Currency'
-        db.create_table(u'cbrcurrencies_currency', (
+        db.create_table(u'cbrcource_currency', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('code', self.gf('django.db.models.fields.CharField')(max_length=3)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=35)),
             ('value', self.gf('django.db.models.fields.DecimalField')(max_digits=30, decimal_places=10)),
         ))
-        db.send_create_signal(u'cbrcurrencies', ['Currency'])
+        db.send_create_signal(u'cbrcource', ['Currency'])
 
 
     def backwards(self, orm):
         # Deleting model 'CurrencyLastUpdate'
-        db.delete_table(u'cbrcurrencies_currencylastupdate')
+        db.delete_table(u'cbrcource_currencylastupdate')
 
         # Deleting model 'Currency'
-        db.delete_table(u'cbrcurrencies_currency')
+        db.delete_table(u'cbrcource_currency')
 
 
     models = {
-        u'cbrcurrencies.currency': {
+        u'cbrcource.currency': {
             'Meta': {'ordering': "('name',)", 'object_name': 'Currency'},
             'code': ('django.db.models.fields.CharField', [], {'max_length': '3'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '35'}),
             'value': ('django.db.models.fields.DecimalField', [], {'max_digits': '30', 'decimal_places': '10'})
         },
-        u'cbrcurrencies.currencylastupdate': {
+        u'cbrcource.currencylastupdate': {
             'Meta': {'object_name': 'CurrencyLastUpdate'},
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'last_update': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'})
         }
     }
 
-    complete_apps = ['cbrcurrencies']
+    complete_apps = ['cbrcource']
